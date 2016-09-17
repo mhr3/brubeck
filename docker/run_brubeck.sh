6a88aaad4e053ca1a2b3b841c43c7fbf2370f4fb
Michal Hruby <michal.hruby@xamarin.com>
2016-09-17 13:50:54 +0100

    Update dockerfile

--- docker/run_brubeck.sh
+++ docker/run_brubeck.sh
@@ -6,12 +6,12 @@ CARBON_HOST=${CARBON_HOST-127.0.0.1}
 CARBON_PORT=${CARBON_PORT-2003}
 CARBON_PICKLE=${CARBON_PICKLE-false}
 
-cat config.template.json \
+cat /opt/brubeck/config.template.json \
     | jq ".samplers[0].port = ${STATSD_PORT}" \
     | jq ".samplers[0].workers = ${STATSD_WORKERS}" \
     | jq ".backends[0].address = \"${CARBON_HOST}\"" \
     | jq ".backends[0].port = ${CARBON_PORT}" \
     | jq ".backends[0].pickle = ${CARBON_PICKLE}" \
-    > config.json
+    > /opt/brubeck/config.json
 
-exec /brubeck/brubeck --log /var/log/brubeck.log --config /config.json
+exec /opt/brubeck/brubeck --log /var/log/brubeck.log --config /opt/brubeck/config.json
